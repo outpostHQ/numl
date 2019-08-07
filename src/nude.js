46d9bc78dfe02f11164d60f12ca8d78898ed3adc
Andrey Yamanov <tenphi@gmail.com>
2019-08-07 12:34:44 +0300

    add feather icons support by default (lazy-load)

--- src/nude.js
+++ src/nude.js
@@ -1,12 +1,15 @@
-import { log } from './helpers';
+import { log, injectScript } from './helpers';
 import css from './css';
 import modifiers from './modifiers';
+
+let featherPromise;
 
 const Nude = {
   modifiers,
   css,
-  iconLoader() {
-    return Promise.resolve('');
+  iconLoader(name) {
+    return (featherPromise || injectScript('https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.22.1/feather.js'))
+      .then(() => window.feather.icons[name].toSvg());
   },
 };
 
