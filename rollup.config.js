0e53bfc6d9c3847faee4ab40fc0ba6815f96311d
Andrey Yamanov <tenphi@gmail.com>
2019-11-05 12:26:29 +0300

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -50,7 +50,7 @@ export default [
     ]
   },
   {
-    input: 'src/frontend.js',
+    input: 'src/index.js',
     external: ['ms'],
     output: [
       { name: 'Nude', file: pkg.module.replace('.js', '.module.js'), format: 'es' }
@@ -70,7 +70,7 @@ export default [
     ]
   },
   {
-    input: 'src/frontend.js',
+    input: 'src/index.js',
     external: ['ms'],
     output: [
       {
