972794b0ffa559ea9e9723a36dd46ccdb3559791
Andrey Yamanov <tenphi@gmail.com>
2020-01-21 19:51:30 +0300

    nu-tab: optimize css definitioin

--- src/elements/tab.js
+++ src/elements/tab.js
@@ -1,5 +1,6 @@
 import NuActiveElement from './activeelement';
 import DirectionMixin from '../mixins/direction';
+import { DIRECTIONS } from '../helpers';
 
 export default class NuTab extends NuActiveElement {
   static get nuTag() {
@@ -22,29 +23,12 @@ export default class NuTab extends NuActiveElement {
          :dir-right[center end]
          :dir-left[center start]`,
       border: `0
-        :dir-bottom:focusable:hover[1b bottom inside color(special)]
-        :dir-bottom:pressed[3b bottom inside color(special)]
-        :dir-bottom:hover:pressed[3b bottom inside color(special)]
-        :dir-bottom:focusable:active[2b bottom inside color(special)]
-        :dir-bottom:focusable:active:hover[2b bottom inside color(special)]
-
-        :dir-right:focusable:hover[1b right inside color(special)]
-        :dir-right:pressed[3b right inside color(special)]
-        :dir-right:hover:pressed[3b right inside color(special)]
-        :dir-right:focusable:active[2b right inside color(special)]
-        :dir-right:focusable:active:hover[2b right inside color(special)]
-
-        :dir-top:focusable:hover[1b top inside color(special)]
-        :dir-top:pressed[3b top inside color(special)]
-        :dir-top:hover:pressed[3b top inside color(special)]
-        :dir-top:focusable:active[2b top inside color(special)]
-        :dir-top:focusable:active:hover[2b top inside color(special)]
-
-        :dir-left:focusable:hover[1b left inside color(special)]
-        :dir-left:pressed[3b left inside color(special)]
-        :dir-left:hover:pressed[3b left inside color(special)]
-        :dir-left:focusable:active[2b left inside color(special)]
-        :dir-left:focusable:active:hover[2b left inside color(special)]`,
+        ${DIRECTIONS.map(dir => `
+          :dir-${dir}:focusable:hover[1b ${dir} inside color(special)]
+          :dir-${dir}:pressed[3b ${dir} inside color(special)]
+          :dir-${dir}:hover:pressed[3b ${dir} inside color(special)]
+          :dir-${dir}:focusable:active[2b ${dir} inside color(special)]
+          :dir-${dir}:focusable:active:hover[2b ${dir} inside color(special)]`)}`,
       hoverable: null,
     };
   }
