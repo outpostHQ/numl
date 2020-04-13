a1a060049c82124463e079cf0a42f59b4a0ba03c
Andrey Yamanov <tenphi@gmail.com>
2020-04-13 20:36:51 +0300

    [size]: add support for custom sizes

--- src/attributes/size.js
+++ src/attributes/size.js
@@ -1,4 +1,5 @@
-import { convertUnit } from '../helpers';
+import { parseAttr } from '../helpers';
+import { injectStyleTag } from '../css';
 
 export const SIZES = {
   xxs: [.666, 1],
@@ -16,27 +17,50 @@ export const SIZES = {
   h6: [1, 1.5],
 };
 
+let PROPS = '';
+
+Object.keys(SIZES).forEach(size => {
+  const arr = SIZES[size];
+
+  PROPS += `--nu-${size}-font-size: ${arr[0]}rem;
+--nu-${size}-line-height: ${arr[1]}rem;${arr[2] ? `
+--nu-${size}-font-weight: ${arr[2]}rem;` : '' }`;
+});
+
+injectStyleTag(`:root{${PROPS}`, 'nu-sizes');
+
 export default function sizeAttr(val) {
-  if (!val) return null;
+  if (!val) val = 'md';
 
-  const tmp = val.split(/\s+/);
-  const values = [];
+  const tmp = val.split(/\s+/).map(vl => parseAttr(val, 1));
+  const styles = {
+    'font-size': 'var(--nu-font-size)',
+    'line-height': 'var(--nu-line-height)',
+  };
 
-  values[0] = SIZES[tmp[0]] ? String(SIZES[tmp[0]][0]) : tmp[0];
-
-  if (!tmp[1] && SIZES[tmp[0]]) {
-    values[1] = String(SIZES[tmp[0]][1]);
+  if (tmp[0].values.length) {
+    styles['--nu-font-size'] = tmp[0].values[0];
+  } else if (tmp[0].mods.length) {
+    styles['--nu-font-size'] = `var(--nu-${tmp[0].mods[0]}-font-size, inherit)`;
   } else {
-    values[1] = SIZES[tmp[1]] ? String(SIZES[tmp[1]][1]) : tmp[1];
+    return;
   }
 
-  const fontSize = convertUnit(values[0]);
-  const lineHeight = values[1] ? convertUnit(values[1]) : 'inherit';
+  if (tmp[1]) {
+    if (tmp[1].values.length) {
+      styles['--nu-line-height'] = tmp[1].values[0];
+    } else if (tmp[1].mods.length) {
+      styles['--nu-line-height'] = `var(--nu-${tmp[1].mods[0]}-line-height, inherit)`;
+    }
+  }
 
-  return {
-    'font-size': fontSize,
-    'line-height': lineHeight,
-    '--nu-font-size': fontSize,
-    '--nu-line-height': lineHeight === 'inherit' ? null : lineHeight,
-  };
+  if (!styles['--nu-line-height']) {
+    if (tmp[0].mods.length) {
+      styles['--nu-line-height'] = `var(--nu-${tmp[0].mods[0]}-line-height, inherit)`;
+    } else {
+      styles['--nu-line-height'] = 'var(--nu-font-size)';
+    }
+  }
+
+  return styles;
 }
