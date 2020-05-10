c5dca3fe9f0598d4b9ae0f457ee72351bf989310
Andrey Yamanov <tenphi@gmail.com>
2020-05-10 19:54:37 +0300

    nu-value: support for daterange

--- src/behaviors/value.js
+++ src/behaviors/value.js
@@ -30,6 +30,14 @@ export default class ValueBehavior extends WidgetBehavior {
 
     if (value instanceof Date) {
       value = `<nu-datetime value="${String(value)}" date></nu-datetime>`;
+    } else if (Array.isArray(value) && value[0] instanceof Date) {
+      value = `
+        <nu-flex gap>
+          <nu-datetime value="${String(value[0])}" date></nu-datetime>
+          <nu-el>–</nu-el>
+          <nu-datetime value="${String(value[1])}" date></nu-datetime>
+        </nu-flex>
+      `;
     } else if (typeof value === 'boolean') {
       value = value ? '<nu-icon name="check"></nu-icon>' : '<nu-icon name="minus"></nu-icon>';
     }
