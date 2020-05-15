6312a18239d2cc01d2125c0037a458bce5b2bfe6
Andrey Yamanov <tenphi@gmail.com>
2020-05-15 18:37:45 +0300

    [shadow]: add special modifier

--- src/attributes/shadow.js
+++ src/attributes/shadow.js
@@ -1,7 +1,12 @@
 import { parseAttr } from '../helpers';
 
+const INTENSITY = 'var(--nu-local-intensity, var(--nu-intensity))';
+const SPECIAL_INTENSITY = 'var(--nu-special-intensity)';
+
 export default function shadowAttr(val) {
-  const { values, color } = parseAttr(val, 1);
+  const { values, mods, color } = parseAttr(val, 1);
+
+  const baseIntensity = mods.includes('special') ? SPECIAL_INTENSITY : INTENSITY;
 
   let x = '0';
   let y = '0';
@@ -36,6 +41,6 @@ export default function shadowAttr(val) {
   }
 
   return {
-    '--nu-local-depth-shadow': `${x} ${y} ${size} ${spread} ${size === '0rem' ? 'rgba(0, 0, 0, 0)' : `rgba(0, 0, 0, calc(var(--nu-local-intensity, var(--nu-intensity)) / ${intensityModifier}))`}`,
+    '--nu-local-depth-shadow': `${x} ${y} ${size} ${spread} ${size === '0rem' ? 'rgba(0, 0, 0, 0)' : `rgba(0, 0, 0, calc(${baseIntensity} / ${intensityModifier}))`}`,
   };
 }
