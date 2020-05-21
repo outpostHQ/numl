fa5ee03199f1a118105e4f28971b5962ecd35b32
Andrey Yamanov <tenphi@gmail.com>
2020-05-21 12:42:08 +0300

    [toggle]: fix default value

--- src/attributes/toggle.js
+++ src/attributes/toggle.js
@@ -2,7 +2,7 @@ import { parseAttr } from '../helpers';
 
 export default function toggleAttr(val) {
   const { values } = parseAttr(val, 1);
-  const size = values[0];
+  const size = values[0] || '0';
   const intensity = values[1] || 'var(--nu-local-intensity, var(--nu-intensity))';
 
   const color = `rgba(0, 0, 0, ${intensity})`;
