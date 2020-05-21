df8c766c7a86512d3f2da82b4f5e763647bbf7cd
Andrey Yamanov <tenphi@gmail.com>
2020-05-21 17:11:57 +0300

    links: smooth navigation by default

--- src/sticky.js
+++ src/sticky.js
@@ -21,6 +21,8 @@ export default function () {
 
         const style = getComputedStyle(el);
         const rect = el.getBoundingClientRect();
+        // Comparison with 1 is required for iOS where rect.y can be positive decimal number like 0.3
+        // Using 0 value will cause in false negative results.
         const sticky = (STICKY_POSITION.includes(style.position) || el.nuHasName('topbar'))
           && !!(scrollTop && rect.y < 1 || scrollLeft && rect.x < 1);
 
