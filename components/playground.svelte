bb1eebb4e0b12b78fe03569e0135a648854044b5
Andrey Yamanov <tenphi@gmail.com>
2020-05-23 20:30:39 +0300

    control: refactoring, improve syntax with negative operator

--- components/playground.svelte
+++ components/playground.svelte
@@ -414,7 +414,7 @@ var b = /wow?/;
     <nu-heading>Collapsing</nu-heading>
     <nu-btn
       checkbox trigger
-      control="collapsing[collapsed]"
+      control="collapsing[!collapsed]"
       t="'Show the block' :pressed['Collapse the block']"></nu-btn>
     <nu-block width="max 20" id="collapsing">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
