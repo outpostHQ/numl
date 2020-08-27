7c1bd77c7fd940b274332f1d0435f0cd18e43efd
Andrey Yamanov <tenphi@gmail.com>
2020-08-27 14:14:35 +0300

    [shadow]: support for named shadows

--- src/attributes/shadow.js
+++ src/attributes/shadow.js
@@ -1,4 +1,5 @@
 import { isNoValue, isYesValue, parseAttr } from '../helpers';
+import { prop } from '../props-helpers';
 
 const SHADOW = 'rgba(var(--nu-shadow-color-rgb), .33)';
 const SPECIAL_SHADOW = 'rgba(var(--nu-special-shadow-color-rgb), .33)';
@@ -18,37 +19,52 @@ export default function shadowAttr(val, defaults, options = {}) {
     val = '0';
   }
 
+  const propName = `--nu-local-${inset ? 'inset' : 'depth'}-shadow`;
+
   let { values, mods, color } = parseAttr(val, 1);
+  let isSpecial = false;
 
-  color = color || (mods.includes('special')
-    ? (specialShadow || SPECIAL_SHADOW)
-    : (shadow || SHADOW));
-
-  let size = defaultValue || '1rem';
-  let x = '0';
-  let y = inset ? '0' : `calc(${size} / 3)`;
-  let spread = '0';
-
-  if (values.length === 1) {
-    size = values[0];
-    y = inset ? '0' : `calc(${size} / 3)`;
-  } else if (values.length >= 2) {
-    x = values[0];
-    y = values[1];
-
-    if (values[2]) {
-      size = values[2];
-    }
-
-    if (values[3]) {
-      spread = values[3];
-    }
+  if (mods.includes('special')) {
+    isSpecial = true;
+    mods = mods.filter(m => m !== 'special');
   }
 
-  const prop = `--nu-local-${inset ? 'inset' : 'depth'}-shadow`;
-  const value = `${x} ${y} ${size} ${spread} ${color}${inset ? ' inset' : ''}`;
+  let value;
+
+  if (mods.length) {
+    const name = mods[0];
+    value = `${prop(`${name}-shadow`)} ${color || ''}${inset ? ' inset' : ''}`;
+  } else {
+    color = color || (isSpecial
+      ? (specialShadow || SPECIAL_SHADOW)
+      : (shadow || SHADOW));
+
+    let size = defaultValue || '1rem';
+    let x = '0';
+    let y = inset ? '0' : `calc(${size} / 3)`;
+    let spread = '0';
+
+    if (values.length === 1) {
+      size = values[0];
+      y = inset ? '0' : `calc(${size} / 3)`;
+    } else if (values.length >= 2) {
+      x = values[0];
+      y = values[1];
+
+      if (values[2]) {
+        size = values[2];
+      }
+
+      if (values[3]) {
+        spread = values[3];
+      }
+    }
+
+    value = `${x} ${y} ${size} ${spread} ${color}${inset ? ' inset' : ''}`;
+  }
+
   const styles = {
-    [prop]: value,
+    [propName]: value,
   };
 
   if (active && mods.includes('active')) {
