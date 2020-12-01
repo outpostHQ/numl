ab7ceefae8baaec3ef2e7c95765ca3fb058c731f
Andrey Yamanov <tenphi@gmail.com>
2020-12-01 10:53:45 +0300

    routing: add setInternalRouter() method

--- src/routing.js
+++ src/routing.js
@@ -4,9 +4,29 @@ export function setRouter(_router) {
   router = _router;
 }
 
+export function setInternalRouter(_router) {
+  router = (url, openNewTab) => {
+    if (
+      openNewTab ||
+      url.startsWith('https://') ||
+      url.includes('//') ||
+      url.startsWith('mailto:')
+    ) {
+      return true;
+    }
+
+    _router(url, openNewTab); // handle routing by Vue Router
+
+    return false;
+  };
+}
+
 const Routing = {
-  route(...args) { return router(...args) },
-  setRouter: setRouter,
+  route(...args) {
+    return router(...args)
+  },
+  setRouter,
+  setInternalRouter,
 };
 
 export default Routing;
