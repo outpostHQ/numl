eb56820e3e584f499cff7d121ddc2b27bb388691
Andrey Yamanov <tenphi@gmail.com>
2021-03-16 15:17:59 +0300

    nu-btngroup: fix disabled styles

--- src/elements/btngroup.js
+++ src/elements/btngroup.js
@@ -12,6 +12,8 @@ export default class NuBtnGroup extends NuGroup {
       border: '',
       fill: 'var(--local-border-color, var(--border-color)) :disabled[rgba(var(--local-border-color-rgb, var(--border-color-rgb)), var(--disabled-opacity))]',
       outline: 'n :radiogroup[focus-inside visible]',
+      transition: 'filter, opacity',
+      filter: 'n :disabled[saturate(0.33) contrast(0.88) opacity(var(--disabled-opacity))]',
     };
   }
 
@@ -20,7 +22,8 @@ export default class NuBtnGroup extends NuGroup {
       'attrs:action': {
         border: '0',
         outline: '^btngroup focus visible :radiogroup[n]',
-        padding: '1x',
+        padding: '1x 1.5x',
+        filter: 'n',
       },
     };
   }
