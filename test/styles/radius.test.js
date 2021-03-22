fc88aa17596d1b92bafd6cc8ac22d15892c3d659
Andrey Yamanov <tenphi@gmail.com>
2021-03-22 16:01:11 +0300

    [radius]: fix tests

--- test/styles/radius.test.js
+++ test/styles/radius.test.js
@@ -2,65 +2,110 @@ import radiusAttr from '../../src/styles/radius';
 import { checkGenerator } from '../utils';
 
 checkGenerator('radius', radiusAttr, {
-  '': {
+  '': [{
     '--local-radius': 'var(--radius)',
     'border-radius': 'var(--local-radius)',
-  },
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'var(--radius)',
+  }],
   'n': undefined,
-  '1r': {
+  '1r': [{
     '--local-radius': 'var(--radius)',
     'border-radius': 'var(--local-radius)',
-  },
-  '2r': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'var(--radius)',
+  }],
+  '2r': [{
     '--local-radius': 'calc(2 * var(--radius))',
     'border-radius': 'var(--local-radius)',
-  },
-  'top': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'calc(2 * var(--radius))',
+  }],
+  'top': [{
     '--local-radius': 'var(--radius) var(--radius) 0 0',
     'border-radius': 'var(--local-radius)',
-  },
-  '1r right': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'var(--radius) var(--radius) 0 0',
+  }],
+  '1r right': [{
     '--local-radius': '0 var(--radius) var(--radius) 0',
     'border-radius': 'var(--local-radius)',
-  },
-  '2r bottom': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': '0 var(--radius) var(--radius) 0',
+  }],
+  '2r bottom': [{
     '--local-radius': '0 0 calc(2 * var(--radius)) calc(2 * var(--radius))',
     'border-radius': 'var(--local-radius)',
-  },
-  'left': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': '0 0 calc(2 * var(--radius)) calc(2 * var(--radius))',
+  }],
+  'left': [{
     '--local-radius': 'var(--radius) 0 0 var(--radius)',
     'border-radius': 'var(--local-radius)',
-  },
-  'round': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'var(--radius) 0 0 var(--radius)',
+  }],
+  'round': [{
     '--local-radius': '9999rem',
     'border-radius': 'var(--local-radius)',
-  },
-  'round top': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': '9999rem',
+  }],
+  'round top': [{
     '--local-radius': '9999rem 9999rem 0 0',
     'border-radius': 'var(--local-radius)',
-  },
-  'ellipse': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': '9999rem 9999rem 0 0',
+  }],
+  'ellipse': [{
     '--local-radius': '50%',
     'border-radius': 'var(--local-radius)',
-  },
-  'ellipse left 2r': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': '50%',
+  }],
+  'ellipse left 2r': [{
     '--local-radius': '50% 0 0 50%',
     'border-radius': 'var(--local-radius)',
-  },
-  'leaf': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': '50% 0 0 50%',
+  }],
+  'leaf': [{
     '--local-radius': 'var(--leaf-sharp-radius) var(--radius) var(--leaf-sharp-radius) var(--radius)',
     'border-radius': 'var(--local-radius)',
-  },
-  'backleaf': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'var(--leaf-sharp-radius) var(--radius) var(--leaf-sharp-radius) var(--radius)',
+  }],
+  'backleaf': [{
     '--local-radius': 'var(--radius) var(--leaf-sharp-radius) var(--radius) var(--leaf-sharp-radius)',
     'border-radius': 'var(--local-radius)',
-  },
-  'leaf 1.5r .5r': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'var(--radius) var(--leaf-sharp-radius) var(--radius) var(--leaf-sharp-radius)',
+  }],
+  'leaf 1.5r .5r': [{
     '--local-radius': 'calc(.5 * var(--radius)) calc(1.5 * var(--radius)) calc(.5 * var(--radius)) calc(1.5 * var(--radius))',
     'border-radius': 'var(--local-radius)',
-  },
-  'backleaf 1.5r .5r': {
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'calc(.5 * var(--radius)) calc(1.5 * var(--radius)) calc(.5 * var(--radius)) calc(1.5 * var(--radius))',
+  }],
+  'backleaf 1.5r .5r': [{
     '--local-radius': 'calc(1.5 * var(--radius)) calc(.5 * var(--radius)) calc(1.5 * var(--radius)) calc(.5 * var(--radius))',
     'border-radius': 'var(--local-radius)',
-  },
+  }, {
+    "$suffix": ">*",
+    '--context-radius': 'calc(1.5 * var(--radius)) calc(.5 * var(--radius)) calc(1.5 * var(--radius)) calc(.5 * var(--radius))',
+  }],
 });
