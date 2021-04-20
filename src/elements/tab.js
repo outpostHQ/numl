ad6050cc08d927c8b0c90cf68bb4e2bdbbd66bb5
Andrey Yamanov <tenphi@gmail.com>
2021-04-20 16:10:29 +0300

    nu-tab: improve styling

--- src/elements/tab.js
+++ src/elements/tab.js
@@ -17,17 +17,20 @@ export default class NuTab extends NuAction {
       gap: '1x',
       items: '@local-tab-items',
       padding: '@local-tab-padding-v @local-tab-padding-h',
-      border: '0',
+      border: '--local-line-width ${direction} bottom inside #special',
       expand: '@local-tab-expand-v @local-tab-expand-h',
       mark: null,
       text: 'sb nowrap',
+      transition: 'border, color',
+      color: '#text :pressed[#special]',
 
       '@local-line-width': `0
         :hover[1bw]
         :pressed[1ow]
-        :hover:pressed[1ow]
         :active[1ow - 1bw]
-        :active:hover[1ow - 1bw]`,
+        :active:hover[1ow - 1bw]
+        :pressed:hover[1ow]
+        :pressed:active.hover[1ow]`,
     };
   }
 }
